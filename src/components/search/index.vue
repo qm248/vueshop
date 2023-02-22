fa47d635752f79a84f33fd58e51ef3d5862bdd43
qm <2481323711@qq.com>
2023-02-22 22:14:00 +0800

    home-firstCommit

--- src/components/search/index.vue
+++ src/components/search/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-show="show.show">123</div>
+  <div class="search-component" v-show="show.show">123</div>
 </template>
 
 <script>
@@ -21,5 +21,13 @@ export default {
 </script>
 
 <style scoped>
-
+.search-component{
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    z-index: 99;
+    left: 0;
+    top: 0;
+    background-color: #ffffff;
+}
 </style>
